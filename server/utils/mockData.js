592d452dfca4b2268d72efeb53243bee67bac3ce
fxola <afolabiabass62@gmail.com>
2019-02-16 15:03:56 +0100

    created meal service and mock data

---
+++ server/utils/mockData.js
@@ -0,0 +1,22 @@
+export default {
+  meals: [
+    {
+      id: 1,
+      name: 'Amala and Ewedu',
+      description: 'Tasty and Palatable',
+      price: '500'
+    },
+    {
+      id: 2,
+      name: 'Afang and Fufu',
+      description: 'Nutritious and Yummy',
+      price: '600'
+    },
+    {
+      id: 3,
+      name: 'Pounded Yam and Egusi',
+      description: 'Nutritious and palatable',
+      price: '700'
+    }
+  ]
+};
