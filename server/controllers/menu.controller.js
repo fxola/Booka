c66c6aac9b9c707848fc2276dc85b2ae1522f8a9
fxola <afolabiabass62@gmail.com>
2019-02-17 04:53:48 +0100

    created menu controller

---
+++ server/controllers/menu.controller.js
@@ -0,0 +1,22 @@
+import MenuService from '../services/menu.service';
+
+class MenuController {
+  static addMenu(req, res) {
+    const newMenu = req.body;
+    const createdMenu = MealService.addMeal(newMenu);
+
+    return res.status(201).json({
+      status: 'successful',
+      data: createdMenu
+    });
+  }
+
+  static fetchMenu(req, res) {
+    const allMenus = MenuService.fetchMenu();
+
+    return res.status(200).json({
+      status: 'success',
+      data: allMenus
+    });
+  }
+}
