781ef69acf89235ab43890a361d752a5bfd1b6ed
fxola <afolabiabass62@gmail.com>
2019-02-21 20:42:10 +0100

    wrote test for POST /meals

--- server/src/tests/meal.test.js
+++ server/src/tests/meal.test.js
@@ -22,7 +22,35 @@ describe('Test for All Meal Endpoints', () => {
         .end((err, res) => {
           expect(res).to.have.status(200);
           expect(res.body).to.be.an('object');
+          expect(res.body.data[0]).to.be.an('object');
           expect(res.body.data[0]).to.have.keys('id', 'name', 'description', 'price');
+          done();
+        });
+    });
+  });
+
+  describe('POST /meals', () => {
+    /**
+     * confirm that the endpoint returns the required status code
+     * confirm that the response body is an object
+     * confirm that the response object contains the required keys
+     */
+    it('should create a new meal and store it in the database', done => {
+      chai
+        .request(app)
+        .post('/api/v1/meals')
+        .set('content-type', 'application/json')
+        .send({
+          id: 5,
+          name: 'New meal option',
+          description: 'Nutritious and palatable',
+          price: '800'
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(201);
+          expect(res.body).to.be.an('object');
+          expect(res.body.data).to.be.an('object');
+          expect(res.body.data).to.have.keys('id', 'name', 'description', 'price');
           done();
         });
     });
