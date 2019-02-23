2f196c7b590bd702247b29318509c3b469199c9b
fxola <afolabiabass62@gmail.com>
2019-02-23 07:37:43 +0100

    updated port

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   return res.send('Up and Running');
