fca6045dbf083ad988f66fea185f9ac1985a8f78
fxola <afolabiabass62@gmail.com>
2019-03-04 19:33:27 +0100

    feature: setup DB connection and environment variables

---
+++ server/src/config/env_config.js
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    use_env_variables: false
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  }
+};
---
+++ server/src/models/index.js
@@ -0,0 +1,47 @@
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
+import envConfig from '../config/env_config';
+
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+
+// eslint-disable-next-line import/no-dynamic-require
+const config = envConfig[env];
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+fs.readdirSync(__dirname)
+  .filter(file => {
+    return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
+  })
+  .forEach(file => {
+    const model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
